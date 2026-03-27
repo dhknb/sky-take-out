c358de26cf2ffcab20cb3f46cc49f7efa3cb5782
dong <dhk11112024@163.com>
2026-03-27 21:20:13 +0800

    新增员工

--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -1,15 +1,18 @@
 package com.sky.service;
 
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
+import com.sky.vo.EmployeeLoginVO;
 
 public interface EmployeeService {
 
     /**
      * 员工登录
-     * @param employeeLoginDTO
+     * @param employeeDTO
      * @return
      */
     Employee login(EmployeeLoginDTO employeeLoginDTO);
 
+    void save(EmployeeDTO employeeDTO);
 }
