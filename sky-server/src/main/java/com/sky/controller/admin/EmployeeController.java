0045eba0179dcd644eef1afa52711134deae85fb
dong <dhk11112024@163.com>
2026-03-28 21:55:38 +0800

    编辑员工

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -3,18 +3,17 @@ package com.sky.controller.admin;
 import com.sky.constant.JwtClaimsConstant;
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import com.sky.properties.JwtProperties;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.EmployeeService;
 import com.sky.utils.JwtUtil;
 import com.sky.vo.EmployeeLoginVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -77,8 +76,31 @@ public class EmployeeController {
         employeeService.save(employeeDTO);
         return Result.success();
     }
+    @GetMapping("/page")
+    public  Result<PageResult> page(EmployeePageQueryDTO employeePageQueryDTO){
+        log.info("分页查询结果 : {}", employeePageQueryDTO);
+         PageResult pageResult= employeeService.page(employeePageQueryDTO);
+        return Result.success(pageResult);
+    }
+    @PostMapping("/status/{status}")
+    public Result startOrStop(@PathVariable Integer status ,Long id){
+        log.info("启用禁用员工账号；{}，{}", status,id);
+        employeeService.startOrStop(status,id);
+        return Result.success();
 
-
+    }
+    @GetMapping("/{id}")
+    public Result<Employee> getById(@PathVariable Long id){
+        log.info("根据id查询员工");
+        Employee employee= employeeService.getById(id);
+        return Result.success(employee);
+    }
+    @PutMapping
+    public Result update(@RequestBody EmployeeDTO employeeDTO){
+        log.info("修改员工信息");
+        employeeService.update(employeeDTO);
+        return Result.success();
+    }
 
 
 }
