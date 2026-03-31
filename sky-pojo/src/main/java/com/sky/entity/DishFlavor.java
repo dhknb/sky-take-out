e815bb0e8fdd298434c573641b1ab0b2c06368de
dong <dhk11112024@163.com>
2026-03-31 20:54:57 +0800

    新增菜品，OSS，aop的实现

--- sky-pojo/src/main/java/com/sky/entity/DishFlavor.java
+++ sky-pojo/src/main/java/com/sky/entity/DishFlavor.java
@@ -19,13 +19,13 @@ public class DishFlavor implements Serializable {
     private static final long serialVersionUID = 1L;
 
     private Long id;
-    //菜品id
+
+    // 菜品id
     private Long dishId;
 
-    //口味名称
+    // 口味名称
     private String name;
 
-    //口味数据list
+    // 口味数据list（JSON字符串）
     private String value;
-
 }
